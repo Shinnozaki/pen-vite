e7cd945b622d5d9f9a714980faf75ec4182cbc07
RhesaDav <rhesadavinanto@ids.id>
2023-10-20 02:29:27 +0700

    fixed card

--- src/components/Gift.tsx
+++ src/components/Gift.tsx
@@ -1,71 +1,102 @@
-import React, { useState, useEffect } from 'react'
-import Navbar from './Navbar'
-import GiftCard from './GiftCard'
+import React, { useState, useEffect } from "react";
+import Navbar from "./Navbar";
+import GiftCard from "./GiftCard";
 
-function Gift() {
-    const [openCard, setOpenCard] = useState(false);
-    const [count, setCount] = useState(0)
-    const [isGifted, setIsGifted] = useState(false)
+const Gift = () => {
+  const [selected, setSelected] = useState<number | null>(null);
+  const [count, setCount] = useState<number>(0);
+  const [value, setValue] = useState<React.ReactNode | null>(null);
 
-    const handleClickBox = () => {
-        setOpenCard(!openCard)
-        // setCount(count +1)
-        // if (count === 3) {
-        //     setIsGifted(true)
-        // }
+  useEffect(() => {
+    if (count === 1) {
+      setValue(data[0].result);
+    } else if (count === 2) {
+      setValue(data[1].result);
+    } else if (count >= 3) {
+      setValue(data[2].result);
     }
+  }, [count]);
 
-    const data = [
-        {
-            id: 1,
-            result:
-                <>
-                    <img src="/gifts/nt1.png" alt="" />
-                    <button className='w-full py-2 bg-[#f3b84f]/50 rounded-full text-white font-semibold text-lg' disabled>{`Yahhh kosong :(`}</button>
-                </>
-        },
-        {
-            id: 2,
-            result:
-                <>
-                    <iframe src="https://youtube.com/embed/RYXKIQ4NFI0?si=Jwza3jsW4VUOTtxl?autoplay=1" allow='autoplay' height="200"></iframe>
-                    <button className='w-full py-2 bg-[#f3b84f]/50 rounded-full text-white font-semibold text-lg' disabled>{`*Emoji menari 3x*`}</button>
-                </>
-        },
-        {
-            id: 3,
-            result:
-                <>
-                    <h1>congrats</h1>
-                    <button className='w-full py-2 bg-[#f3b84f]/50 rounded-full text-white font-semibold text-lg'>{`hore`}</button>
-                </>
-        }
-    ]
+  const handleClickBox = (id: number) => {
+    setSelected(id);
+    setCount(count + 1);
+  };
 
-    return (
-        <div className="w-screen h-screen bg-[#474284]">
-            <Navbar />
-            <div className="w-screen mt-10 flex flex-col items-center">
-                <div className="flex flex-col gap-5 text-center mb-10">
-                    <h1 className="text-white font-bold text-5xl">CLAIM YOUR GIFT</h1>
-                    <div>
-                        <p className="text-white font-normal text-lg">Click on one of the buttons to claim!</p>
-                        <p className="text-white font-normal text-lg">But choose wisely... you only have <b>ONE</b> chance.</p>
-                    </div>
-                </div>
+  const data = [
+    {
+      id: 1,
+      result: (
+        <>
+          <img src="/gifts/nt1.png" alt="" />
+          <button
+            className="w-full py-2 bg-[#f3b84f]/50 rounded-full text-white font-semibold text-lg"
+            disabled
+          >
+            {`Yahhh kosong :(`}
+          </button>
+        </>
+      ),
+    },
+    {
+      id: 2,
+      result: (
+        <>
+          <iframe
+            src="https://youtube.com/embed/RYXKIQ4NFI0?si=Jwza3jsW4VUOTtxl?autoplay=1"
+            allow="autoplay"
+            height="200"
+          ></iframe>
+          <button
+            className="w-full py-2 bg-[#f3b84f]/50 rounded-full text-white font-semibold text-lg"
+            disabled
+          >
+            {`*Emoji menari 3x*`}
+          </button>
+        </>
+      ),
+    },
+    {
+      id: 3,
+      result: (
+        <>
+          <h1>congrats</h1>
+          <button className="w-full py-2 bg-[#f3b84f]/50 rounded-full text-white font-semibold text-lg">{`hore`}</button>
+        </>
+      ),
+    },
+  ];
 
-                <h1 className='text-white'>Count : {count}</h1>
-                <div className="bg-[#5a53ad] p-10 shadow-lg flex gap-10 rounded-lg">
-                    {data.map((item, index) => (
-                        <GiftCard open={openCard} onClick={handleClickBox} key={index}>  
-                            {item.result}
-                        </GiftCard>
-                    ))}
-
-                </div>
-            </div>
-
+  return (
+    <div className="w-screen h-screen bg-[#474284]">
+      <Navbar />
+      <div className="w-screen mt-10 flex flex-col items-center">
+        <div className="flex flex-col gap-5 text-center mb-10">
+          <h1 className="text-white font-bold text-5xl">CLAIM YOUR GIFT</h1>
+          <div>
+            <p className="text-white font-normal text-lg">
+              Click on one of the buttons to claim!
+            </p>
+            <p className="text-white font-normal text-lg">
+              But choose wisely... you only have <b>ONE</b> chance.
+            </p>
+          </div>
         </div>
-    )
-}
-export default Gift
+
+        <h1 className="text-white">Count: {count}</h1>
+        <div className="bg-[#5a53ad] p-10 shadow-lg flex gap-10 rounded-lg">
+          {data.map((item) => (
+            <GiftCard
+              key={item.id}
+              open={selected === item.id}
+              onClick={() => handleClickBox(item.id)}
+            >
+              {value}
+            </GiftCard>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Gift;
--- src/components/GiftCard.tsx
+++ src/components/GiftCard.tsx
@@ -1,44 +1,50 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 
 interface GiftProps {
-    children: React.ReactNode;
-    open: boolean;
-    onClick?: () => void;
+  children: React.ReactNode;
+  open: boolean;
+  onClick?: () => void;
 }
 
-const GiftCard = ({open, children, onClick}: GiftProps) => {
-    // const [isFlipped, setIsFlipped] = useState(open)
-    // const [isAnimating, setIsAnimating] = useState(open)
-    
-    // function handleFlip() {
-    //     if (!isAnimating) {
-    //         setIsFlipped(true)
-    //         setIsAnimating(true)
-    //     }
-    // }
+const GiftCard = ({ open, children, onClick }: GiftProps) => {
+//   const [isFlipped, setIsFlipped] = useState(open);
+  const [isAnimating, setIsAnimating] = useState(false);
 
-    return (
-        <>
-            <div className='flip-card w-[300px] h-[350px]'>
-                <motion.div
-                    className='bg-gray-200/50 flex flex-col items-center justify-between rounded-lg hover:shadow-lg flip-card-inner w-[100%] h-[100%]'
-                    initial={false}
-                    animate={{ rotateY: open ? 180 : 360 }}
-                    transition={{ duration: 1 }}
-                    onAnimationComplete={onClick}
-                >
-                    <div className='flip-card-front w-[100%] h-[100%] flex flex-col items-center p-5 justify-between'>
-                        <img src="/gifts/unopenned.png" alt="" className='w-[250px]' />
-                        <button className='w-full py-2 bg-[#f3b84f] rounded-full text-white font-semibold text-lg shadow-md hover:bg-[#c79744]' onClick={onClick}>Choose me</button>
-                    </div>
-                    <div className='flip-card-back w-[100%] h-[100%] flex flex-col items-center p-5 justify-between'>
-                        {children} 
-                    </div>
-                </motion.div>
-            </div>
-        </>
-    )
-}
+  const handleFlip = () => {
+    if (!isAnimating && open) {
+        setIsAnimating(true);
+    }
+  };
+
+  const onAnimationComplete = () => {
+    setIsAnimating(false);
+  };
+
+  return (
+    <div className="flip-card w-[300px] h-[350px]" onClick={handleFlip}>
+      <motion.div
+        className="bg-gray-200/50 flex flex-col items-center justify-between rounded-lg hover:shadow-lg flip-card-inner w-[100%] h-[100%]"
+        initial={false}
+        animate={{ rotateY: open ? 180 : 0 }}
+        transition={{ duration: 1 }}
+        onAnimationComplete={onAnimationComplete}
+      >
+        <div className="flip-card-front w-[100%] h-[100%] flex flex-col items-center p-5 justify-between">
+          <img src="/gifts/unopenned.png" alt="" className="w-[250px]" />
+          <button
+            className="w-full py-2 bg-[#f3b84f] rounded-full text-white font-semibold text-lg shadow-md hover:bg-[#c79744]"
+            onClick={onClick}
+          >
+            Choose me
+          </button>
+        </div>
+        <div className="flip-card-back w-[100%] h-[100%] flex flex-col items-center p-5 justify-between">
+          {children}
+        </div>
+      </motion.div>
+    </div>
+  );
+};
 
 export default GiftCard;
